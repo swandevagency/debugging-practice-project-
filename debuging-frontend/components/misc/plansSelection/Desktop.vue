18c00939fc4f0b83845991c891ef2e3f7d44ac13
Arsam Bagheramiri <arsam5ba5@gmail.com>
2022-09-06 13:27:49 +0430

    nality an'plan selection d style debugg

--- debuging-frontend/components/misc/plansSelection/Desktop.vue
+++ debuging-frontend/components/misc/plansSelection/Desktop.vue
@@ -1,39 +1,31 @@
 <template>
     <div class="plan-container">
-        <form class="plan-container-options" action="#">
-            <p>
+        <form class="plan-container-form" action="#">
                 <input type="radio" id="plan1" name="plan-selection" checked>
-                <label @click="getCoordinate1()" for="plan1" ref="plan1">
+                <p @click="getCoordinate" for="plan1" ref="firstPlan">
                     e-commerce
-                </label>
-            </p>
-            <p>
-                <input type="radio" id="plan2" name="plan-selection">
-                <label @click="getCoordinate2()" for="plan2" ref="plan2">
-                    Portfolio
-                </label>
-            </p>
-            <p>
-                <input type="radio" id="plan3" name="plan-selection">
-                <label @click="getCoordinate3()" for="plan3" ref="plan3">
-                    Service
-                </label>
-            </p>
-            <p>
-                <input type="radio" id="plan4" name="plan-selection">
-                <label @click="getCoordinate4()" for="plan4" ref="plan4">
-                    e-commerce
-                </label>
-            </p>
-            <p>
-                <input type="radio" id="plan5" name="plan-selection">
-                <label @click="getCoordinate5()" for="plan5" ref="plan5">
-                    Test-site
-                </label>
-            </p>
-            
-            <div class="animation-rectangle"  ref="rectangle"></div>
+                </p>
 
+                <input type="radio" id="plan2" name="plan-selection">
+                <p @click="getCoordinate" for="plan2">
+                    Portfolio
+                </p>
+
+                <input type="radio" id="plan3" name="plan-selection">
+                <p @click="getCoordinate" for="plan3">
+                    Service
+                </p>
+
+                <input type="radio" id="plan4" name="plan-selection">
+                <p @click="getCoordinate" for="plan4">
+                    e-commerce
+                </p>
+
+                <input type="radio" id="plan5" name="plan-selection">
+                <p @click="getCoordinate" for="plan5">
+                    Test-site
+                </p>
+            <div class="animation-rectangle"  ref="rectangle"></div>
         </form>
     </div>
 </template>
@@ -41,53 +33,19 @@
 <script>
 export default {
     mounted() {
-        let plan = this.$refs.plan1;
         let rectangle = this.$refs.rectangle;
-        const width = plan.offsetWidth;
+        // defining rectangle width at frist
+        const width = this.$refs.firstPlan.offsetWidth;
         rectangle.style.width = width + 'px'
     },
     methods:{
-        getCoordinate1() {
-            let plan = this.$refs.plan1;
+        getCoordinate(e) {
             let rectangle = this.$refs.rectangle;
-            rectangle.style.left = plan.offsetLeft + 'px';
-            rectangle.style.animation = 'bounce' + '0.5s' + 'linear';
-            const width = plan.offsetWidth;
+            // position the rectangle by the position of element
+            rectangle.style.left = e.target.offsetLeft + 'px';
+            // defining the rectangle width by text width + padding
+            const width = e.target.offsetWidth;
             rectangle.style.width = width + 'px'
-
-        },
-        getCoordinate2() {
-            let plan = this.$refs.plan2;
-            let rectangle = this.$refs.rectangle;
-            rectangle.style.left = plan.offsetLeft + 'px';
-            rectangle.style.animation = 'bounce 0.5s linear';
-            const width = plan.offsetWidth 
-            rectangle.style.width = width + 'px'
-            
-        },
-        getCoordinate3() {
-            let plan = this.$refs.plan3;
-            let rectangle = this.$refs.rectangle;
-            rectangle.style.left = plan.offsetLeft + 'px';
-            const width = plan.offsetWidth;
-            rectangle.style.width = width + 'px'
-
-        },
-        getCoordinate4() {
-            let plan = this.$refs.plan4;
-            let rectangle = this.$refs.rectangle;
-            rectangle.style.left = plan.offsetLeft + 'px';
-            const width = plan.offsetWidth;
-            rectangle.style.width = width + 'px'
-
-        },
-        getCoordinate5() {
-            let plan = this.$refs.plan5;
-            let rectangle = this.$refs.rectangle;
-            rectangle.style.left = plan.offsetLeft + 'px';
-            const width = plan.offsetWidth;
-            rectangle.style.width = width + 'px'
-
         },
     }
 }
@@ -106,7 +64,7 @@ export default {
         justify-content: center;
     }
 }
-.plan-container-options{
+.plan-container-form{
     position: relative;
     background: $bg-secondary;
     border-radius: 50px;
@@ -115,18 +73,19 @@ export default {
     justify-content: space-between;
     align-items: center;
     width: 85%;
-    padding: 17px 5px;
+    padding: 0px 5px;
     margin: 50px 0px;
-
+    
     p{
         z-index: 5;
-    }
-
-    label{
         cursor: pointer;
         position: relative;
         z-index: 5;
         padding: 0px 15px;
+        height: 65px;
+        display: flex;
+        align-items: center;
+        user-select: none;
     }
 
     input{
@@ -141,12 +100,7 @@ export default {
     background-color: #fff;
     border-radius: 50px;
     box-shadow: 11px 7px 5px rgba(22, 68, 45, 0.01), 6px 4px 4px rgba(22, 68, 45, 0.02), 3px 2px 3px rgba(22, 68, 45, 0.04), 1px 0px 2px rgba(22, 68, 45, 0.04), 0px 0px 0px rgba(22, 68, 45, 0.04);
-    transition: all 0.2s ease;
+    transition: all 0.2s cubic-bezier(.08,1,.88,1.16);
 }
 
-@keyframes bounce { 
-    0% { transform: translateX(0); } 
-    50% { transform: translateX(4px); } 
-    100% { transform: translateX(0px); } 
-}
 </style>
